91ac71c08ef51394292463bc0c5c8770e5fae4f3
YanCheng Lin <nick841005@gmail.com>
2017-06-05 19:06:39 +0800

    test2

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,10 +1,14 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWorld!");
+		for(int i = 0; i < 10; i++)
+			System.out.println(new Random().nextInt(2));
 	}
-
 }
